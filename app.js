06e746a7f0bdb7f93b7b6997ed943ee88cb8da5c
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-04-18 01:05:29 +0500

    box hover changing colors

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ let boxes = document.querySelectorAll('.box');
 // console.log(boxes);
 let move = "circle";
 info.innerHTML = "Circle goes first"
+// let boxHover = Array.from(boxes)
+
+
 
 
 boxes.forEach((cell, index) => {
     cell.id = index;
     // console.log(cell);
+    cell.addEventListener('mouseenter', () => {
+        cell.classList.add(`boxHoverFor${move}`)
+    })
+    cell.addEventListener('mouseleave', () => {
+        cell.classList.remove(`boxHoverFor${move}`)
+    })
     cell.addEventListener('click', yourMove);
 })
 
 function yourMove(element) {
     // console.log(element.target)
-
     const moveDisplay = document.createElement('div')
     moveDisplay.classList.add(move);
     element.target.append(moveDisplay);
     if (move !== 'circle') {
         move = 'circle'
+
     } else {
         move = 'cross'
     }
+    // hoverBoxes(move)
     info.innerHTML = `It is now ${move}'s turn`
     element.target.removeEventListener('click', yourMove)
     checkScore();
+    console.log(move)
+    return move
 }
 
 
@@ -38,29 +50,29 @@ function checkScore() {
 
 
     winningCombos.forEach(array => {
-        const circleWins = array.every(cell => 
+        const circleWins = array.every(cell =>
             allBoxes[cell].firstChild?.classList.contains('circle'));
 
-            
-            if (circleWins) {
-                info.innerHTML = `Circle Wins`
-                allBoxes.forEach(box => 
-                    box.replaceWith(box.cloneNode(true)));
-                return
-            }
-        
+
+        if (circleWins) {
+            info.innerHTML = `Circle Wins`
+            allBoxes.forEach(box =>
+                box.replaceWith(box.cloneNode(true)));
+            return
+        }
+
     })
 
     winningCombos.forEach(array => {
-        const crossWins = array.every(cell => 
+        const crossWins = array.every(cell =>
             allBoxes[cell].firstChild?.classList.contains('cross'));
-            if (crossWins) {
-                info.innerHTML = `Cross Wins`
-                allBoxes.forEach(box => 
-                    box.replaceWith(box.cloneNode(true)));
-                return
-            }
-        
-    
+        if (crossWins) {
+            info.innerHTML = `Cross Wins`
+            allBoxes.forEach(box =>
+                box.replaceWith(box.cloneNode(true)));
+            return
+        }
+
+
     })
 }
